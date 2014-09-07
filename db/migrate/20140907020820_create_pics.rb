5d87bde5d6e6d4934b1d942fecefe4ce29e2210f
Todd <laugh_aloud80026@yahoo.com>
2014-09-07 02:22:34 +0000

    Create the pic model and add to database

---
+++ db/migrate/20140907020820_create_pics.rb
@@ -0,0 +1,11 @@
+class CreatePics < ActiveRecord::Migration
+  def change
+    create_table :pics do |t|
+    	t.integer :lesson
+    	t.string :emotion
+    	t.text :learned
+
+      t.timestamps
+    end
+  end
+end
