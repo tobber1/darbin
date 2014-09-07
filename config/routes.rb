c9946d26bb517698b751695edd8675c5d34410ad
Todd <laugh_aloud80026@yahoo.com>
2014-09-07 03:38:25 +0000

    First half of Dynamic List Page lesson

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Darbin::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'static_page#index'
-
+   resources :pics
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
